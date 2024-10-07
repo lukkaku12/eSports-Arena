86117c2cfbf4f8b36504369deab15e6e262585f0
Lucas <lucasaristiz789@gmail.com>
2024-10-07 16:31:35 -0500

    FEAT: login added, finishing extra entitiesof the project

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,9 +1,20 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Request, UseGuards } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ApiTags } from '@nestjs/swagger';
+import { LocalAuthGuard } from './guards/local-auth.guard';
 
 @ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
+
+
+  @UseGuards(LocalAuthGuard) // Usa el LocalAuthGuard para el login
+  @Post('login')
+  async login(@Request() req) {
+    console.log(req.user)
+    return this.authService.login(req.user);
+     // El usuario ya ha sido validado
+  }
+
 }
--- src/results/entities/result.entity.ts
+++ src/results/entities/result.entity.ts
@@ -1,1 +1,26 @@
-export class Result {}
+import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
+
+@Entity()
+export class Result {
+    
+    @PrimaryGeneratedColumn()
+    id: number;
+
+    @Column()
+    match_id: number;
+
+    @Column()
+    winner_id: number;
+
+    @Column()
+    loser_id: number;
+
+    @Column()
+    winner_score: number;
+
+    @Column()
+    loser_score: number;
+
+    @Column({ type: 'timestamp', default: () => 'CURRENT_TIMESTAMP' })
+    created_at: Date;
+}
